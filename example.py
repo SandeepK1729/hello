484cc43f3f1937348187cdc88bea52bc6dec575d
Sandeep <sandeepkumargalipelly@gmail.com>
2023-01-24 14:37:20 +0000

    fixed conflict

--- example.py
+++ example.py
@@ -2,7 +2,6 @@
 
 def git_opeation():
  """This is about git operation"""
->>>>>>> 2e8ab10d543834477ac6057ece703dce48ab4e42
  print("I am adding example.py file to the remote repository.")
 git_opeation()
 
